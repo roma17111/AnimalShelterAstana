f953114f77d43dc6a765b568a05994c91f1ec258
roman <romanze1706@gmail.com>
2023-02-28 20:00:03 +0300

    test

---
+++ src/main/java/animal/shelter/animalsshelter/AnimalsShelterApplication.java
@@ -0,0 +1,13 @@
+package animal.shelter.animalsshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalsShelterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/animal/shelter/animalsshelter/AnimalsShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package animal.shelter.animalsshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
