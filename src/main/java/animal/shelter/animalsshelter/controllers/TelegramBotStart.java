eba794baf14a1e7fa957435bf2d34a26c254fa93
roman <romanze1706@gmail.com>
2023-03-02 20:40:50 +0300

    Создан меню-метод

--- src/main/java/animal/shelter/animalsshelter/controllers/TelegramBotStart.java
+++ src/main/java/animal/shelter/animalsshelter/controllers/TelegramBotStart.java
@@ -17,12 +17,15 @@ import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageMe
 import org.telegram.telegrambots.meta.api.objects.File;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.meta.updateshandlers.SentCallback;
 
 import javax.swing.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -30,10 +33,16 @@ import java.util.concurrent.CompletableFuture;
 @Component
 public class TelegramBotStart extends TelegramLongPollingBot {
 
+    private final static String INFO_BUTTON = "INFO_BUTTON";
+    private final static String NECESSARY = "NECESSARY_TO_GET_ANIMAL";
+    private final static String SEND_REPORT = "SEND_REPORT";
+    private final static String CALL_VOLUNTEER = "CALL_VOLUNTEER";
+
     @Value("${bot.name}")
     private String botName;
     @Value("${bot.key}")
     private String botKey;
+
     @Override
     public String getBotUsername() {
         return botName;
@@ -46,16 +55,23 @@ public class TelegramBotStart extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-            Message message = update.getMessage();
+        Message message = update.getMessage();
         long id = update.getMessage().getChatId();
-        if (update.hasMessage()&& message.hasText()) {
-            sendBotMessage(id,"Вы ввели - " + message.getText());
-            System.out.println(message.getText());
-            log.info(id+" "+message.getText());
+        if (update.hasMessage()&&message.hasText()) {
+            switch (message.getText()) {
+                case "/start":
+                    sendBotMessage(id,getBotUsername());
+                    break;
+                default:
+                    sendBotMessage(id, "Вы ввели - " + message.getText());
+                    System.out.println(message.getText());
+                    log.info(id + " " + message.getText());
+                    break;
+            }
         }
     }
 
-    private void sendBotMessage(long id,String name) {
+    private void sendBotMessage(long id, String name) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(String.valueOf(id));
         sendMessage.setText(name);
@@ -64,5 +80,37 @@ public class TelegramBotStart extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void getBotStartUserMenu() {
+        SendMessage sendMessage = new SendMessage();
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+        List<InlineKeyboardButton> row2 = new ArrayList<>();
+        List<InlineKeyboardButton> row3 = new ArrayList<>();
+        InlineKeyboardButton shelterInfoButton = new InlineKeyboardButton();
+        shelterInfoButton.setText("Информация о приюте");
+        shelterInfoButton.setCallbackData(INFO_BUTTON);
+        InlineKeyboardButton necessary = new InlineKeyboardButton();
+        necessary.setText("Какие условия необходимо выполнить, чтобы взять животное");
+        necessary.setCallbackData(NECESSARY);
+        InlineKeyboardButton report = new InlineKeyboardButton();
+        report.setText("Отправить отчёт о содержании животного");
+        report.setCallbackData(SEND_REPORT);
+        InlineKeyboardButton call = new InlineKeyboardButton();
+        call.setText("Позвать волонтёра");
+        call.setCallbackData(CALL_VOLUNTEER);
+        row.add(shelterInfoButton);
+        row1.add(necessary);
+        row2.add(report);
+        row3.add(call);
+        rows.add(row);
+        rows.add(row1);
+        rows.add(row2);
+        rows.add(row3);
+        inlineKeyboardMarkup.setKeyboard(rows);
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
     }
 }
