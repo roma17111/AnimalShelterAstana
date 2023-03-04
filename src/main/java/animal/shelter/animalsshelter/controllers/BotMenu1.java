b2fe37b28282bdae85448eb3ba2291629e46c1b2
roman <romanze1706@gmail.com>
2023-03-04 13:47:24 +0300

    Созданы кнопки меню этап1

--- src/main/java/animal/shelter/animalsshelter/controllers/BotMenu1.java
+++ src/main/java/animal/shelter/animalsshelter/controllers/BotMenu1.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,7 +36,42 @@ public class BotMenu1 extends TelegramBotStart{
         List<InlineKeyboardButton> row3 = new ArrayList<>();
         List<InlineKeyboardButton> row4 = new ArrayList<>();
         List<InlineKeyboardButton> row5 = new ArrayList<>();
-
+        InlineKeyboardButton tellMe = new InlineKeyboardButton();
+        tellMe.setText("Расскажи о себе");
+        tellMe.setCallbackData(TELL_ABOUT_SHELTER);
+        InlineKeyboardButton cLockWork = new InlineKeyboardButton();
+        cLockWork.setText("Часы работы");
+        cLockWork.setCallbackData(WORK_TIME);
+        InlineKeyboardButton addressShelter = new InlineKeyboardButton();
+        addressShelter.setText("Адрес и схема проезда");
+        addressShelter.setCallbackData(ADDRESS);
+        InlineKeyboardButton recommendations = new InlineKeyboardButton();
+        recommendations.setText("Техника безопасности");
+        recommendations.setCallbackData(SECURITY);
+        InlineKeyboardButton registerMe = new InlineKeyboardButton();
+        registerMe.setText("Оставить контактные данные");
+        registerMe.setCallbackData(REGISTRATION);
+        InlineKeyboardButton volunteerCall = new InlineKeyboardButton();
+        volunteerCall.setText("Вопрос к волонтёру");
+        volunteerCall.setCallbackData(CALL_VOLUNTEER);
+        row1.add(tellMe);
+        row2.add(cLockWork);
+        row2.add(addressShelter);
+        row3.add(recommendations);
+        row4.add(registerMe);
+        row5.add(volunteerCall);
+        rows.add(row1);
+        rows.add(row2);
+        rows.add(row3);
+        rows.add(row4);
+        rows.add(row5);
+        inlineKeyboardMarkup.setKeyboard(rows);
+        editMessageText.setReplyMarkup(inlineKeyboardMarkup);
+        try {
+            execute(editMessageText);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
 
     }
 }
