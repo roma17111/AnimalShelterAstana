3d8c07565f8412119b8a58ee6fb66dd1a4a26c35
Altanim <altanim0404@gmail.com>
2023-03-07 13:08:30 +0300

    add volunteer and call volunteer message classes

---
+++ src/main/java/animal/shelter/animalsshelter/model/CallVolunteerMsg.java
@@ -0,0 +1,59 @@
+package animal.shelter.animalsshelter.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "callVolunteerMsg")
+public class CallVolunteerMsg {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "msg_id")
+    private long id;
+
+    @Column(name = "msg_date")
+    private LocalDateTime msgDate;
+
+    @Column(name = "msg_text")
+    private String msgText;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "volunteer_id")
+    private Volunteer volunteer;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getMsgDate() {
+        return msgDate;
+    }
+
+    public void setMsgDate(LocalDateTime msgDate) {
+        this.msgDate = msgDate;
+    }
+
+    public String getMsgText() {
+        return msgText;
+    }
+
+    public void setMsgText(String msgText) {
+        this.msgText = msgText;
+    }
+
+    public Volunteer getVolunteer() {
+        return volunteer;
+    }
+
+    public void setVolunteer(Volunteer volunteer) {
+        this.volunteer = volunteer;
+    }
+}
