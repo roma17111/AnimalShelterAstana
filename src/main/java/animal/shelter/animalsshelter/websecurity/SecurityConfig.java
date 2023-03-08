af33ec98e1037d6257c5fcc6b1e9934d6c38a8d2
roman <romanze1706@gmail.com>
2023-03-08 23:20:26 +0300

    Добавил spring security

--- src/main/java/animal/shelter/animalsshelter/websecurity/SecurityConfig.java
+++ src/main/java/animal/shelter/animalsshelter/websecurity/SecurityConfig.java
@@ -1,11 +1,26 @@
 package animal.shelter.animalsshelter.websecurity;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Admin")
+                .password("12345")
+                .authorities("ROLE_USER")
+               ;
+    }
 }
