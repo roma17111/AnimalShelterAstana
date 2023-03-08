4fcc430d6e47a48ffec6396ec4f2d9e20c0a721b
roman <romanze1706@gmail.com>
2023-03-08 23:04:14 +0300

    подключены библиотеки spring security

---
+++ src/main/java/animal/shelter/animalsshelter/websecurity/SecurityConfig.java
@@ -0,0 +1,7 @@
+package animal.shelter.animalsshelter.websecurity;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+}
