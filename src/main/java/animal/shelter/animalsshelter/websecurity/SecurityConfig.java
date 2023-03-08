7471178e1072a5b33d4b24dba7afe3f824d0345c
roman <romanze1706@gmail.com>
2023-03-08 23:12:15 +0300

    ...

--- src/main/java/animal/shelter/animalsshelter/websecurity/SecurityConfig.java
+++ src/main/java/animal/shelter/animalsshelter/websecurity/SecurityConfig.java
@@ -1,7 +1,11 @@
 package animal.shelter.animalsshelter.websecurity;
 
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 }
