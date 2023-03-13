7b7b0e233123756b130d3edae6ff2202088d6a2e
roman <romanze1706@gmail.com>
2023-03-13 21:47:19 +0300

    fixes

--- src/main/java/animal/shelter/animalsshelter/controllers/TelegramBotStart.java
+++ src/main/java/animal/shelter/animalsshelter/controllers/TelegramBotStart.java
@@ -568,7 +568,7 @@ public class TelegramBotStart extends TelegramLongPollingBot {
                 .filter(user1 -> user1.isNotified() == true)
                 .collect(Collectors.toList());
         for (User user1 : userList) {
-            if (update.getMessage().getChatId() != user1.getChatId()) {
+            if (update.getMessage().getChatId() != user1.getChatId()&&user1.isNotified()==true) {
                 sendBotMessage(user1.getChatId(),"Волонтёр: "+
                         update.getMessage().getChat().getFirstName()+"\n"+update.getMessage().getText());
             }
