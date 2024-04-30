28f202cf287313d339aef3da46fa23dc7eb06594
Roman Yakimenko <romanze1706@gmail.com>
2024-04-30 17:00:28 +0300

    fixe spring security

--- src/main/java/animal/shelter/animalsshelter/config/security/Security.java
+++ src/main/java/animal/shelter/animalsshelter/config/security/Security.java
@@ -14,17 +14,19 @@ import org.springframework.security.web.context.AbstractSecurityWebApplicationIn
 @EnableWebSecurity
 public class Security   extends WebSecurityConfigurerAdapter {
 
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
         auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("Admin").password("69701").roles("ADMIN"));
+                .withUser(userBuilder.username("admin").password("12345").roles("ADMIN"));
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests()
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
                 .antMatchers(HttpMethod.GET).hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST).hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT).hasRole("ADMIN")
